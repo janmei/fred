d7eeb5835d98929769ce35c7095c81f95c517b85
Jan Meininghaus <hey@janmeininghaus.com>
2020-07-13 19:24:54 +0200

    first commit

---
+++ index.py
@@ -0,0 +1,42 @@
+import requests
+
+import paho.mqtt.client as mqtt
+
+def startRequest(bri):
+    url = "http://192.168.0.20/api/vWOsvVaprpwthxcEnlbcxVjhU6deEL1JV7X8PnXj/lights/3/state"
+
+    payload = "{\"bri\": %s}" % bri
+    headers = {
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("PUT", url, headers=headers, data = payload)
+
+    print(response.text.encode('utf8'))
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+    client.subscribe("test/#")
+
+def on_message(client, userdata, msg):
+    payload = str(msg.payload, "utf-8")
+    print(payload)
+    if payload.isnumeric():
+        print("Number")
+        startRequest(payload)
+        print(msg.topic + " " + payload)
+        pass
+    else:
+        print("Not a Number")
+        pass
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60)
+
+client.loop_forever()
+
+
